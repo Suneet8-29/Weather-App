7636a5a2ac17145134a33bc7d57149729d007252
Suneet Jena <suneetjena@Suneets-MacBook-Air.local>
2020-06-17 20:00:03 +0530

    UI Changes

--- public/js/app.js
+++ public/js/app.js
@@ -14,17 +14,30 @@ console.log("Cool !! Its working");
 //     console.log(data.features[0]);
 // })
 
-const asyncf = async () => {
+var searchText = document.querySelector('#searchText');
+var dataLocation = document.querySelector('#dataLocation');
+var dataForecast = document.querySelector('#dataForecast');
+var btnSearchCall = document.getElementById('btnSearch');
+const asyncf = async (dataLocation, dataForecast, searchText) => {
     try {
-        var response = await fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/hyderabad.json?access_token=pk.eyJ1Ijoic3VuZWV0amVuYSIsImEiOiJja2I3ZzNtaTkwNWZvMnp0b28xa3NkamNpIn0.MT7wE2e0jpzdZFO5pqQ4zg&limit=1');
-        var data = await response.json();
+        dataForecast.textContent = "";
+        dataLocation.textContent = "Loading....";
+        alert(searchText.value);
+        var response = await fetch('/weather?search=' + searchText.value);
+        var result = await response.json();
 
-        console.log(data.features[0]);
+        dataLocation.textContent = "Location : " + result.location;
+        dataForecast.textContent = "Forecast : " + result.forecast;
     } catch (error) {
-        console.log(error);
+        dataLocation.textContent = error;
     }
 
 
 }
 
-asyncf();
+
+btnSearchCall.addEventListener('click', (e) => {
+    e.preventDefault();
+    asyncf(dataLocation, dataForecast, searchText);
+
+})
